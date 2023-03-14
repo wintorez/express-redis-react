0febcc2249e5e742a996f8f8fcbc5bdea4ee3bf6
Reza Sadr <wintorez@gmail.com>
2023-03-14 10:32:39 -0400

    Add redis to dev environment

--- server/index.js
+++ server/index.js
@@ -4,23 +4,14 @@ const redis = require('redis')
 const cors = require('cors')
 const logger = require('morgan')
 
+require('dotenv').config()
+
 const app = express()
 const port = process.env.PORT || 3001
+const redisUrl = process.env.REDIS_URL ?? 'redis://redis:6379'
 
 app.use(cors())
 app.use(logger('dev'))
-
-let redisClient
-
-if (process.env.NODE_ENV === 'production') {
-  ;(async () => {
-    redisClient = redis.createClient({ url: 'redis://redis:6379' })
-
-    redisClient.on('error', (error) => console.error(`Error: ${error}`))
-
-    await redisClient.connect()
-  })()
-}
 
 async function fetchApiData(species) {
   const url = `https://www.fishwatch.gov/api/species/${species}`
@@ -33,13 +24,12 @@ async function fetchApiData(species) {
 
 async function getCachedData(req, res, next) {
   const { species } = req.params
-  let results
 
   try {
     const cacheResults = await redisClient.get(species)
 
     if (cacheResults) {
-      results = JSON.parse(cacheResults)
+      const results = JSON.parse(cacheResults)
 
       console.log('Results retrieved from cache')
 
@@ -55,38 +45,35 @@ async function getCachedData(req, res, next) {
 
 async function getSpeciesData(req, res) {
   const { species } = req.params
-  let results
 
   try {
-    results = await fetchApiData(species)
+    const results = await fetchApiData(species)
 
     if (results.length === 0) {
       throw new Error('API returned an empty array!')
     }
 
-    if (process.env.NODE_ENV === 'production') {
-      await redisClient.set(species, JSON.stringify(results), {
-        EX: 180,
-        NX: true,
-      })
-    }
-
-    res.send({
-      fromCache: false,
-      data: results,
+    await redisClient.set(species, JSON.stringify(results), {
+      EX: 180,
+      NX: true,
     })
+
+    res.send({ fromCache: false, data: results })
   } catch (error) {
     console.error(error)
     res.status(404).send('Data unavailable!')
   }
 }
 
-if (process.env.NODE_ENV === 'production') {
-  app.get('/fish/:species', getCachedData, getSpeciesData)
-} else {
-  app.get('/fish/:species', getSpeciesData)
-}
+app.get('/fish/:species', getCachedData, getSpeciesData)
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
+const redisClient = redis.createClient({ url: redisUrl })
+redisClient.on('error', (error) => console.error(`Error: ${error}`))
+
+redisClient.connect().then(() => {
+  console.log('Cache server connection successful')
+
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+  })
 })
